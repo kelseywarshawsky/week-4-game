b13078f517af100c773e6fe6a138d05ae5b75f60
Kelsey Gasser <kjgasser@gmail.com>
2018-02-09 21:00:23 -0600

    Add files via upload
    For Ryan to check

---
+++ assets/javascript/game.js
@@ -0,0 +1,34 @@
+var targetNumber = 111;
+
+$("#target-score").text(targetNumber);
+var counter = 0;
+
+var numberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+var crystals = ["assets/pink.png", "assets/green.png", "assets/blue.png", "assets/purple.png"]
+
+for (var i = 0; i < crystals.length; i++) {
+    var gem= $("<img>"); 
+    gem.attr("src", crystals[i]);
+    gem.addClass("gem-image");
+    gem.attr("data-gemvalue", numberOptions[Math.floor(Math.random() * numberOptions.length)]);
+    $(".col-md-8").append(gem);
+
+}
+
+  $(".gem-image").on("click", function () {
+    
+    var gemValue = ($(this).attr("data-gemvalue"));
+    gemValue = parseInt(gemValue);
+
+    counter += gemValue;
+
+    alert("New score: " + counter);
+
+    if (counter === targetNumber) {
+      alert("You win!");
+    } else if (counter >= targetNumber) {
+      alert("You lose!!");
+    }
+
+
+  });
